6cd743f8486d8fbf2aa17cb2834f7a4b39116285
dryprj <dryprj@gmail.com>
2023-04-18 12:10:49 +0300

    Added Admin Layout, List and Add Category for Admin side

--- MvcProject/Controllers/AdminCategoryController.cs
+++ MvcProject/Controllers/AdminCategoryController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using FluentValidation.Results;
+using MvcProject.BusinessLayer.Concrete;
+using MvcProject.BusinessLayer.ValidationRules;
+using MvcProject.DataAccessLayer.EntityFramework;
+using MvcProject.EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +13,39 @@ namespace MvcProject.Controllers
 {
     public class AdminCategoryController : Controller
     {
-        // GET: AdminCategory
+        CategoryManager cm = new CategoryManager(new EFCategoryDal());
+
+        [HttpGet]
         public ActionResult Index()
         {
+            var categoryValues = cm.GetCategoryList();
+            return View(categoryValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category cat)
+        {
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult results = cv.Validate(cat);
+
+            if (results.IsValid)
+            {
+                cm.CategoryAddBL(cat);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
     }
