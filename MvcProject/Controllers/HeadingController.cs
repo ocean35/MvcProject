7b37eedea86370906c6710195c2810ed23fb776a
dryprj <dryprj@gmail.com>
2023-04-21 22:31:32 +0300

    HeadingController and AddHeading

--- MvcProject/Controllers/HeadingController.cs
+++ MvcProject/Controllers/HeadingController.cs
@@ -1,8 +1,10 @@
 ﻿using MvcProject.BusinessLayer.Concrete;
 using MvcProject.DataAccessLayer.EntityFramework;
+using MvcProject.EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,11 +14,42 @@ namespace MvcProject.Controllers
     {
         // GET: Heading
         HeadingManager hm = new HeadingManager(new EFHeadingDal());
-
+        CategoryManager cm = new CategoryManager(new EFCategoryDal());
+        WriterManager wm = new WriterManager(new EFWriterDal());
         public ActionResult Index()
         {
             var geadingValues = hm.GetHeadingList();
             return View(geadingValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valueCategory = (from x in cm.GetCategoryList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.ID.ToString()
+                                                  }).ToList();
+
+            List<SelectListItem> valueWriter = (from x in wm.GetWriterList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.WriterName + " " + x.WriterSurname + " - " + x.WriterTitle,
+                                                    Value = x.ID.ToString()
+                                                }).ToList();
+
+            ViewBag.vlc = valueCategory;
+            ViewBag.vlwriter = valueWriter;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading heading)
+        {
+            hm.HeadingAddBL(heading);
+            return RedirectToAction("Index");
         }
     }
 }
