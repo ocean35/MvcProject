58494663019e52940bb947056cc2dbcb13346f6a
dryprj <dryprj@gmail.com>
2023-04-24 14:29:29 +0300

    Writer Configuration and Index page updated fro images

--- MvcProject.DataAccessLayer/Concrete/Configurations/WriterConfiguration.cs
+++ MvcProject.DataAccessLayer/Concrete/Configurations/WriterConfiguration.cs
@@ -14,7 +14,7 @@ namespace MvcProject.DataAccessLayer.Concrete.Configurations
         {
             this.Property(x => x.WriterName).HasMaxLength(50);
             this.Property(x => x.WriterSurname).HasMaxLength(50);
-            this.Property(x => x.WriterImage).HasMaxLength(100);
+            this.Property(x => x.WriterImage).HasMaxLength(250);
             this.Property(x => x.WriterAbout).HasMaxLength(100);
             this.Property(x => x.WriterEmail).HasMaxLength(200);
             this.Property(x => x.WriterPassword).HasMaxLength(200);
